36f3a85d054c6c5f1f756968e11503b809a0907e
Apple <apple@Apples-MacBook-Pro-7.local>
2019-06-20 14:43:44 -0700

    new files added

---
+++ main.js
@@ -0,0 +1,54 @@
+var hyper = document.getElementsByClassName(".hyper");
+var normal = document.getElementsByClassName(".normal");
+var lazy = document.getElementsByClassName(".lazy");
+var large = document.getElementsByClassName(".large");
+var medium = document.getElementsByClassName(".medium");
+var small = document.getElementsByClassName(".small");
+var noShedding = document.getElementsByClassName(".noShedding");
+var okWithShedding = document.getElementsByClassName(".okWithShedding");
+var btn = $('.button')
+
+btn.on("click",chooseDog);
+
+function chooseDog(){
+  if (hyper.checked === true) {
+    console.log("chooseDog");
+    window.location.href = "dogs.html";
+}else if (hyper.checked === true && large.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (hyper.checked === true && medium.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (hyper.checked === true && medium.checked === true && okWithShedding.checked === true) {
+  //dog breed
+}else if (hyper.checked === true && small.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (hyper.checked === true && small.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && large.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && large.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && medium.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && medium.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && small.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (normal.checked === true && small.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (lazy.checked === true && large.checked === true && noShedding.checked === true){
+  //dog breed
+}else if (lazy.checked === true && large.checked === true && okWithShedding.checked === true){
+  //dog breed
+}else if (lazy.checked === true && medium.checked === true && noShedding.checked === true) {
+  //dog breed
+}else if (lazy.checked === true && medium.checked === true && okWithShedding.checked === true) {
+  //dog breed
+}else if (lazy.checked === true && small.checked === true && noShedding.checked === true) {
+  //dog breed
+}else if (lazy.checked === true && small.checked === true && okWithShedding.checked === true) {
+  //dog breed
+  } else {
+  //invalid input
+  }
+}
