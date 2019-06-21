19c5fdd83eda866eae6635a8867ef702ab8db332
Apple <apple@Apples-MacBook-Pro-7.local>
2019-06-21 10:59:54 -0700

    committing new files

--- main.js
+++ main.js
@@ -1,55 +1,72 @@
-var hyper = document.getElementsByClassName(".hyper");
-var normal = document.getElementsByClassName(".normal");
-var lazy = document.getElementsByClassName(".lazy");
-var large = document.getElementsByClassName(".large");
-var medium = document.getElementsByClassName(".medium");
-var small = document.getElementsByClassName(".small");
-var noShedding = document.getElementsByClassName(".noShedding");
-var okWithShedding = document.getElementsByClassName(".okWithShedding");
+var hyper = document.querySelector(".hyper");
+var normal = document.querySelector(".normal");
+var lazy = document.querySelector(".lazy");
+var large = document.querySelector(".large");
+var medium = document.querySelector(".medium");
+var small = document.querySelector(".small");
+var noShedding = document.querySelector(".noShedding");
+var okWithShedding = document.querySelector(".okWithShedding");
 var btn = $('.button')
 
 btn.on("click",chooseDog);
 
 function chooseDog(){
-  if (hyper.checked === true) {
-    window.location.href = "dogs.html";
-    console.log("chooseDog");
+if (hyper.checked === true && large.checked === true && noShedding.checked === true) {
 
+  window.location.href = "hyper-large-noShedding.html";
 }else if (hyper.checked === true && large.checked === true && okWithShedding.checked === true){
-  //dog breed
+
+  window.location.href = "hyper-large-shed.html";
+
 }else if (hyper.checked === true && medium.checked === true && noShedding.checked === true){
-  //dog breed
+
+  window.location.href = "hyper-medium-noShedding.html";
 }else if (hyper.checked === true && medium.checked === true && okWithShedding.checked === true) {
-  //dog breed
+
+  window.location.href = "hyper-medium-shed.html";
 }else if (hyper.checked === true && small.checked === true && noShedding.checked === true){
-  //dog breed
+
+  window.location.href = "hyper-small-noShed.html";
 }else if (hyper.checked === true && small.checked === true && okWithShedding.checked === true){
-  //dog breed
+
+  window.location.href = "hyper-small-shed.html";
 }else if (normal.checked === true && large.checked === true && noShedding.checked === true){
-  //dog breed
+
+  window.location.href = "normal-large-noShed.html";
 }else if (normal.checked === true && large.checked === true && okWithShedding.checked === true){
-  //dog breed
+
+  window.location.href = "normal-large-shed.html";
 }else if (normal.checked === true && medium.checked === true && noShedding.checked === true){
   //dog breed
+  window.location.href = "normal-medium-noShed.html";
 }else if (normal.checked === true && medium.checked === true && okWithShedding.checked === true){
   //dog breed
+  window.location.href = "normal-medium-shed.html";
 }else if (normal.checked === true && small.checked === true && noShedding.checked === true){
   //dog breed
+  window.location.href= "normal-small-noShed.html";
 }else if (normal.checked === true && small.checked === true && okWithShedding.checked === true){
   //dog breed
+  window.location.href = "normal-small-shed.html";
 }else if (lazy.checked === true && large.checked === true && noShedding.checked === true){
   //dog breed
+  window.location.href = "lazy-large-noShed.html";
 }else if (lazy.checked === true && large.checked === true && okWithShedding.checked === true){
   //dog breed
+  window.location.href = "lazy-large-shed.html";
 }else if (lazy.checked === true && medium.checked === true && noShedding.checked === true) {
   //dog breed
+  window.location.href = "lazy-medium-noShed.html";
 }else if (lazy.checked === true && medium.checked === true && okWithShedding.checked === true) {
   //dog breed
+  window.location.href = "lazy-medium-shed.html";
 }else if (lazy.checked === true && small.checked === true && noShedding.checked === true) {
   //dog breed
+  window.location.href = "lazy-small-noShed.html";
 }else if (lazy.checked === true && small.checked === true && okWithShedding.checked === true) {
   //dog breed
+  window.location.href = "lazy-small-shed.html";
   } else {
-  //invalid input
+  (`Answer invalid`);
   }
 }
